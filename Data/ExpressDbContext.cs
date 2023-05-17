d543cf8d9ed52eae207cbe3e69e4a05400479d4a
2E4O8T <benoit.froidevan@axa.fr>
2023-05-17 12:34:22 +0200

    premier commit

---
+++ Data/ExpressDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Express.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Express.Data
+{
+    public class ExpressDbContext : IdentityDbContext
+    {
+        public ExpressDbContext(DbContextOptions<ExpressDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Inventaire> Inventaires { get; set; }
+        public DbSet<Reparation> Reparations { get; set; }
+    }
+}
